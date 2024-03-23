c434415e8fd36cf3ac893ce6977c6820407c775d
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-03-23 13:17:32 +0300

    Добавил расположение кнопок в меню

--- markup/markup.py
+++ markup/markup.py
@@ -1,5 +1,5 @@
 # импортируем специальные типы телеграм бота для создания элементов интерфейса
-from telebot.types import KeyboardButton
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 from core import setting
 from data_base.dbalchemy import DBManager
 
@@ -18,3 +18,36 @@ class Keyboards:
         Создает и возвращает кнопку по входным параметрам
         """
         return KeyboardButton(setting.KEYBOARD[name])
+
+    def start_menu(self):
+        """
+        Создает разметку кнопок в основном меню и возвращает разметку
+        """
+        self.markup = ReplyKeyboardMarkup(True, True)
+        itm_btn_1 = self.set_btn('CHOOSE_GOODS')
+        itm_btn_2 = self.set_btn('INFO')
+        itm_btn_3 = self.set_btn('SETTINGS')
+        # рассположение кнопок в меню
+        self.markup.row(itm_btn_1)
+        self.markup.row(itm_btn_2, itm_btn_3)
+        return self.markup
+
+    def info_menu(self):
+        """
+        Создает разметку кнопок в меню 'О магазине'
+        """
+        self.markup = ReplyKeyboardMarkup(True, True)
+        itm_btn_1 = self.set_btn('<<')
+        # рассположение кнопок в меню
+        self.markup.row(itm_btn_1)
+        return self.markup
+
+    def settings_menu(self):
+        """
+        Создает разметку кнопок в меню 'Настройки'
+        """
+        self.markup = ReplyKeyboardMarkup(True, True)
+        itm_btn_1 = self.set_btn('<<')
+        # рассположение кнопок в меню
+        self.markup.row(itm_btn_1)
+        return self.markup

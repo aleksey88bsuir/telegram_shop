aa29144a1baa441e5147fa91e97180260a6dd9ea
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-03-23 13:22:54 +0300

    Сделал рефактор названия файлов

--- handlers/handler_main.py
+++ handlers/handler_main.py
@@ -1,10 +1,20 @@
+from .handler_all_text import HandlerAllText
+from .handler_commands import HandlerCommands
+
+
 class HandlerMain:
     """
     Класс компоновщик
     """
     def __init__(self, bot):
+        # получаем объект нашего бота
         self.bot = bot
         # здесь будет инициализация обработчиков
+        self.handler_commands = HandlerCommands(self.bot)
+        self.handler_all_text = HandlerAllText(self.bot)
 
     def handle(self):
-        pass
+        # здесь будет запуск обработчиков
+        self.handler_commands.handle()
+        self.handler_all_text.handle()
+
